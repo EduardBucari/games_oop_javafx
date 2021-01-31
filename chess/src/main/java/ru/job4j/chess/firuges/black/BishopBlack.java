7ea204bc0bada36e8fd91ac8ef5677a415c4188f
Eduard Bucari <eduard.bucari@gmail.com>
2021-02-01 00:38:55 +0300

    2. Каркас шахматной доски [#361054]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -1,5 +1,6 @@
 package ru.job4j.chess.firuges.black;
 
+import ru.job4j.chess.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
@@ -16,10 +17,25 @@ public class BishopBlack implements Figure {
     }
 
     @Override
+    // 3. Задача реализовать метод way таким образом,
+    // чтобы он вернул массив из четырех клеток: D2, E3, F4, G5.
+    // Чтобы заполнить массив ячеек, нужно использовать метод Cell.findBy
     public Cell[] way(Cell dest) {
-        throw new IllegalStateException(
-                String.format("Could not way by diagonal from %s to %s", position, dest)
-        );
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not move by diagonal from %s to %s", position, dest)
+            );
+        }
+        int size = 4;
+        Cell[] steps = new Cell[size];
+        int deltaX = +1-1;
+        int deltaY = -1-1;
+        int x = index + deltaX; // вычислить ячейку через index + deltaX
+        int y = index + deltaY; // вычистить ячейку через index + deltaY
+        for (int index = 0; index < size; index++) {
+            steps[index] = Cell.findBy(x, y);
+        }
+        return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.Test;
+import ru.job4j.chess.firuges.Cell;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class BishopBlackTest {
+
+    @Test
+    //  1. Проверка метода position. Создать объект и вызвать у него метод position.
+    //   Проверьте, что он занимает ту же ячейку, что и при создании объекта.
+    public void testPosition() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        bishopBlack.position();
+    }
+
+  // 2. Проверка метода copy. Создайте объект и вызовите у него метод copy.
+  // Проверить, что, возвращенный объект имеет правильную позицию.
+    public void testCopy() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.F8);
+        bishopBlack.copy(Cell.F8);
+    }
+
+    // 3. Создать тест-метод для проверки метода way. Для этого создайте объект с начальным положением C1.
+    // Вызовите метод way G5. На данном этапе метод еще не реализован и ваша задача реализовать
+    // его таким образом, чтобы он вернул массив из четырех клеток: D2, E3, F4, G5.
+    public void testWay() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        bishopBlack.way(Cell.G5);
+    }
+
+    public void testIsDiagonal() {
+    }
+
+
+}
