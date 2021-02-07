335d099d4a0a1440e89316c7de5dcabd170e941a
Eduard Bucari <eduard.bucari@gmail.com>
2021-02-08 01:51:44 +0300

    2. Каркас шахматной доски [#361054](12)

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -3,6 +3,7 @@ package ru.job4j.chess;
 import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class LogicTest {
@@ -11,8 +12,9 @@ public class LogicTest {
     @Test(expected = FigureNotFoundException.class)
     public void move()
             throws FigureNotFoundException {
+        //Для этого теста надо начинать ход с другой клетки, Т.е. когда фигура не найдена.
         Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
+        logic.add(new BishopBlack(null));
         logic.move(Cell.C1, Cell.H6);
 
     }
@@ -20,16 +22,18 @@ public class LogicTest {
     @Test(expected = OccupiedCellException.class)
     public void move()
             throws OccupiedCellException {
+        // Для этого теста должно быть 2 фигуры, чтобы вторая стояла на пути следвания первой фигуры
         Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
+        logic.add(new BishopBlack(Cell.G5));
         logic.move(Cell.C1, Cell.H6);
     }
 
     @Test(expected = ImpossibleMoveException.class)
     public void move()
             throws ImpossibleMoveException {
+        // Для этого теста фигура должна ходить не по диагонали
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.H6);
+        logic.move(Cell.C1, Cell.G4);
     }
 }
